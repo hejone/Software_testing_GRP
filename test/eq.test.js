0b7274407231e20557b4afda5d34142970f3664b
Joni Heinonen <joni.heinonen@gmail.com>
2020-11-27 10:16:18 +0200

    Improved 'eq.js' tests

--- test/eq.test.js
+++ test/eq.test.js
@@ -4,11 +4,11 @@ const expect = chai.expect;
 
 
 describe('Eq.js', () => {
+	const number = 3;
+	const string = number.toString();
+	const array = [number];
+	const object = {number: number};
     describe('Testing equality of different types', () => {
-		const number = Math.random() * 1000;
-		const string = number.toString();
-		const array = [number];
-		const object = {number: number};	
 		it('should evaluate two of the same type and value as equal', () => {
 			expect(eq(number, number)).to.be.true;
 			expect(eq(string, string)).to.be.true;
@@ -19,17 +19,33 @@ describe('Eq.js', () => {
 			expect(eq(number, string)).to.be.true;
 			expect(eq(number, array)).to.be.true;
 			expect(eq(string, object.number)).to.be.true;
-			expect(eq(object.number, array));
+			expect(eq(object.number, array)).to.be.true;
 		});
 		it('should evaluate NaN, null and undefined as equal with same type', () => {
 			const nan = Number.NaN;
-			const null_val = null;
-			const undefined_val = undefined;
 			expect(eq(nan, nan)).to.be.true; // Not in vanilla JS
-			expect(eq(null_val, null_val)).to.be.true;
-			expect(eq(undefined_val, undefined_val)).to.be.true;
-			expect(eq(null_val, undefined_val)).to.be.true;
-			expect(eq(nan, undefined_val)).to.be.false;
+			expect(eq(null, null)).to.be.true;
+			expect(eq(undefined, undefined)).to.be.true;
+			expect(eq(null, undefined)).to.be.true;
+			expect(eq(nan, undefined)).to.be.false;
 		});
-    });
+	});
+	describe('Testing inequality of different types', () => {
+		const number2 = 4;
+		const string2 = number2.toString();
+		const array2 = [number2];
+		const object2 = {number: number2};
+		it('should evaluate two of the same type and but different value as inequal', () => {
+			expect(eq(number, number2)).to.be.false;
+			expect(eq(string, string2)).to.be.false;
+			expect(eq(array, array2)).to.be.false;
+			expect(eq(object, object2)).to.be.false;
+		});
+		it('should evaluate two other types as inequal when value is different', () => {
+			expect(eq(number2, string)).to.be.false;
+			expect(eq(number2, array)).to.be.false;
+			expect(eq(string2, object.number)).to.be.false;
+			expect(eq(object2.number, array)).to.be.false;
+		});
+	});
 });
